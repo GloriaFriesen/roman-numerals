33e71212f2a1b64de433dc428c95a742d70c812e
Gloria Friesen and Grace Stuart <grace@>
2017-02-23 12:02:48 -0800

    add arabicValue array

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,20 @@ var output = []
 var romanValue [
   ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"] // ONES
   ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"] //TENS
-  ["C", "CC", "CCC", "CD" "D", "DC", "DCC", "DCCC", "CM"]//HUNDREDS
+  ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]//HUNDREDS
   ["M", "MM", "MMM"]//THOUSANDS
 ]
 
+var arabicValue [
+  ["1", "2", "3", "4", "5", "6", "7", "8", "9"] //ONES
+  ["10", "20", "30", "40", "50", "60", "70", "80", "90"] //TENS
+  ["100", "200", "300", "400", "500", "600", "700", "800", "900" ] //HUNDREDS
+  ["1000", "2000", "3000"] //THOUSANDS
+]
+
 var romanNumeral = function(userInput) {
-  if (userInput === 1) {
-    output = "i";
+  if (userInput <= 9) {
+    output = romanValue[0, ];
     console.log(output);
     return output;
   }
