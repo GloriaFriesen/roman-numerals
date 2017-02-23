e6dabf6295487e4cb4c341b5014d853829953131
Gloria Friesen and Grace Stuart <grace@>
2017-02-23 13:51:39 -0800

    add function to return the romanValue.

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,23 @@ var romanValue = [
 ]
 
 var arabicValue = [
-  ["1", "2", "3", "4", "5", "6", "7", "8", "9"], //ONES
+  [1, 2, 3, 4, 5, 6, 7, 8, 9], //ONES
   ["10", "20", "30", "40", "50", "60", "70", "80", "90"], //TENS
   ["100", "200", "300", "400", "500", "600", "700", "800", "900" ], //HUNDREDS
   ["1000", "2000", "3000"] //THOUSANDS
 ]
 
 var romanNumeral = function(userInput) {
+debugger;
   if (userInput <= 9) {
-    var inputIndex = arabicValue.findIndex(userInput);
-    console.log(inputIndex);
-    output = romanValue[0][inputIndex];
-    console.log(output);
+      function inputIndex(arabicValue) {
+      return arabicValue[0] === userInput;
+    }
+    var arabicIndex = arabicValue.findIndex(inputIndex);
+    output = romanValue[0][arabicIndex];
+
+    var romanEqual = romanValue[0][output];
+    console.log(romanEqual);
   }
 };
 
