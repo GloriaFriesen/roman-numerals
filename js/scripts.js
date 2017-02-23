7d9e9d95b8e325d25764cb1139165ac8db9d6795
Gloria Friesen and Grace Stuart <grace@>
2017-02-23 13:24:29 -0800

    add findIndex method on arabicValue with userInput

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
 var output = []
 
-var romanValue [
-  ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"] // ONES
-  ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"] //TENS
-  ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]//HUNDREDS
+var romanValue = [
+  ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], // ONES
+  ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], //TENS
+  ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],//HUNDREDS
   ["M", "MM", "MMM"]//THOUSANDS
 ]
 
-var arabicValue [
-  ["1", "2", "3", "4", "5", "6", "7", "8", "9"] //ONES
-  ["10", "20", "30", "40", "50", "60", "70", "80", "90"] //TENS
-  ["100", "200", "300", "400", "500", "600", "700", "800", "900" ] //HUNDREDS
+var arabicValue = [
+  ["1", "2", "3", "4", "5", "6", "7", "8", "9"], //ONES
+  ["10", "20", "30", "40", "50", "60", "70", "80", "90"], //TENS
+  ["100", "200", "300", "400", "500", "600", "700", "800", "900" ], //HUNDREDS
   ["1000", "2000", "3000"] //THOUSANDS
 ]
 
 var romanNumeral = function(userInput) {
   if (userInput <= 9) {
-    output = romanValue[0, ];
+    var inputIndex = arabicValue.findIndex(userInput);
+    console.log(inputIndex);
+    output = romanValue[0][inputIndex];
     console.log(output);
-    return output;
   }
 };
 
