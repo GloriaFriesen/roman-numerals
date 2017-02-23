3994e920c74bbf0ac0aa1b82ffb053dfe7e1bbe9
Gloria Friesen and Grace Stuart <grace@>
2017-02-23 10:17:48 -0800

    add var for roman numerals.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
+var output = ""
+var one = "I"
+var five = "V"
+var ten = "X"
+var fifty = "L"
+var oneHundred = "C"
+var fiveHundred = "D"
+var oneThousand = "M"
 
+var romanNumeral = function(userInput) {
+  if (userInput === 1) {
 
+    return output();
+  };
 
 $(document).ready(function() {
   $("#romanNumerals").submit(function() {
     event.preventDefault();
     var userInput = parseInt($("input#userInput").val());
-    console.log(userInput);
   });
 });
