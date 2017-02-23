192606bbf5dfb636019bc927cfcf3aaa47114574
Gloria Friesen and Grace Stuart <grace@>
2017-02-23 09:50:20 -0800

    add doc.ready, and var to receive parsed user input.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+
+$(document).ready(function() {
+  $("#romanNumerals").submit(function() {
+    event.preventDefault();
+    var userInput = parseInt($("input#userInput").val());
+
+  });
+
+});
